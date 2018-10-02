5aa23b3c206d66574018c759adc23a561b9ec988
Flc <four_leaf_clover@foxmail.com>
2018-10-02 22:55:22 +0800

    add config

---
+++ config/elasticsearch.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * Elasticsearch 配置文件
+ */
+return [
+    // 默认连接名
+    'default' => 'default',
+
+    // 所有连接
+    'connections' => [
+        'default' => ['192.168.0.211:9200'],
+    ],
+];
--- src/ElasticsearchServiceProvider.php
+++ src/ElasticsearchServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Flc\Laravel\Elasticsearch;
 
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 
 /**
  * Elasticsearch 服务者
@@ -11,6 +13,24 @@ use Illuminate\Support\ServiceProvider;
  */
 class ElasticsearchServiceProvider extends ServiceProvider
 {
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $source = realpath($raw = __DIR__ . '/../config/elasticsearch.php') ?: $raw;
+
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+            $this->publishes([$source => config_path('elasticsearch.php')]);
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('elasticsearch');
+        }
+
+        $this->mergeConfigFrom($source, 'elasticsearch');
+    }
+
     /**
      * Register the service provider.
      *
