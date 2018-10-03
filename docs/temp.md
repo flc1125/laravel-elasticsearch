8332cdff5a4147dfcf3e3b0f2fe0e1a98e73d2ce
Flc <four_leaf_clover@foxmail.com>
2018-10-03 21:41:32 +0800

    backup

---
+++ docs/temp.md
@@ -0,0 +1,5 @@
+## 查询指定文档ID
+
+```php
+Elasticsearch::index('songs')->find('JUrrOWYBxQtmaSRNoBCM', ['trans_param','pay_type']);
+```
--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -4,6 +4,7 @@ namespace Flc\Laravel\Elasticsearch\Query;
 
 use Elasticsearch\Client as ElasticsearchClient;
 use Illuminate\Database\Concerns\BuildsQueries;
+use InvalidArgumentException;
 
 /**
  * Elasticsearch 查询构建类
@@ -33,7 +34,7 @@ class Builder
      *
      * @var string
      */
-    protected $type;
+    protected $type = 'doc';
 
     /**
      * 需要查询的字段
@@ -105,68 +106,87 @@ class Builder
     }
 
     /**
-     * 返回数据
+     * 通过文档 ID 查询数据
      *
-     * @return Collection
-     */
-    public function get()
-    {
-        return collect($this->processSelect());
-    }
-
-    /**
-     * 处理搜寻结果
+     * @param mixed $id
+     * @param array $columns
      *
-     * @return array
+     * @return mixed
      */
-    protected function processSelect()
+    public function find($id, $columns = ['*'])
     {
-        $result = $this->runSelect();
+        $params = [
+            'index'   => $this->index,
+            'type'    => $this->type,
+            'id'      => $id,
+            '_source' => $columns,
+        ];
 
-        // 格式化数据...
-
-        return $result;
-    }
-
-    /**
-     * 执行搜寻
-     *
-     * @return array
-     */
-    protected function runSelect()
-    {
-        return $this->client->search(
-            $this->toParams()
+        return $this->runExtract(
+            $this->client->get($params)
         );
     }
 
     /**
-     * 执行搜索的参数
+     * 执行格式化输出数据
+     *
+     * @param array $result
+     * @param array $fields 如为单个，则直接返回数组
      *
      * @return array
      */
-    protected function toParams()
+    protected function runExtract($result = [], $fields = ['_source'])
     {
-        $params = [
-            'index' => $this->index,
-            'from'  => 0,
-            'size'  => 20,
-            'body'  => [
-                'query' => [
-                    'bool' => $this->wheres,
-                ],
-                // '_source' => ['*'],
-                'sort' => $this->orders,
-            ],
-        ];
+        $data = [];
 
-        if ($this->type) {
-            $params['type'] = $this->type;
+        $fields = is_string($fields) ? explode(',', $fields) : $fields;
+
+        foreach ($fields as $field) {
+            if (! isset($result[$field])) {
+                throw new InvalidArgumentException("[{$field} not found]");
+            }
+
+            $data[$field] = $result[$field];
         }
+
+        if (count($data) == 1) {
+            return collect(reset($data));
+        }
+
+        return collect($data);
     }
 
-    public function info()
-    {
-        return $this->client->info();
-    }
+    /*
+     * 返回数据
+     *
+     * @return Collection
+     */
+    // public function get()
+    // {
+    //     return collect($this->runSearch());
+    // }
+
+    /*
+     * 执行搜寻
+     *
+     * @return array
+     */
+    // protected function runSearch()
+    // {
+    //     $params = [
+    //         'index' => $this->index,
+    //         'type'  => $this->type,
+    //         'from'  => 0,
+    //         'size'  => 20,
+    //         'body'  => [
+    //             'query' => [
+    //                 'bool' => $this->wheres,
+    //             ],
+    //             // '_source' => ['*'],
+    //             'sort' => $this->orders,
+    //         ],
+    //     ];
+
+    //     return $this->client->search($params);
+    // }
 }
