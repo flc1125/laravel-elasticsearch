751c78f8cb504b05e4c7082de2c737b22137a621
Flc <four_leaf_clover@foxmail.com>
2018-10-03 11:54:54 +0800

    builder init

--- src/ElasticsearchConnection.php
+++ src/ElasticsearchConnection.php
@@ -2,7 +2,8 @@
 
 namespace Flc\Laravel\Elasticsearch;
 
-use Elasticsearch\Client as ElasticsearchRawClient;
+use Elasticsearch\Client as ElasticsearchClient;
+use Flc\Laravel\Elasticsearch\Query\Builder;
 
 /**
  * Elasticsearch 连接
@@ -12,18 +13,45 @@ use Elasticsearch\Client as ElasticsearchRawClient;
 class ElasticsearchConnection
 {
     /**
-     * @var ElasticsearchRawClient $client
+     * @var ElasticsearchClient $client
      */
     protected $client;
 
     /**
      * 创建一个Elasticsearch 连接
      *
-     * @param ElasticsearchRawClient $client
+     * @param ElasticsearchClient $client
      */
-    public function __construct(ElasticsearchRawClient $client)
+    public function __construct(ElasticsearchClient $client)
     {
         $this->client = $client;
+    }
+
+    /**
+     * 指定索引或索引数据
+     *
+     * @param  string|array $value
+     * @return \Flc\Laravel\Elasticsearch\Query\Builder|array
+     */
+    public function index($value)
+    {
+        if (is_string($value)) {
+            return $this->builder()->index($value);
+        }
+
+        return $this->client->index($value);
+    }
+
+    /**
+     * 调用构建类
+     *
+     * @return \Flc\Laravel\Elasticsearch\Query\Builder
+     */
+    public function builder()
+    {
+        return new Builder(
+            $this->client
+        );
     }
 
     /**
--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -1,10 +1,91 @@
 <?php
 
+namespace Flc\Laravel\Elasticsearch\Query;
+
+use Elasticsearch\Client as ElasticsearchClient;
+
+/**
+ * Elasticsearch 查询构建类
+ *
+ * @author Flc <i@flc.io>
+ */
 class Builder
 {
+    /**
+     * Elasticsearch Client
+     *
+     * @var \Elasticsearch\Client
+     */
+    protected $client;
 
-    function __construct(argument)
+    /**
+     * 索引名
+     *
+     * @var string
+     */
+    protected $index;
+
+    /**
+     * 索引Type
+     *
+     * @var string
+     */
+    protected $type = 'docs';
+
+    /**
+     * 搜寻条件
+     *
+     * @var array
+     */
+    public $wheres = [
+        'must'     => [],
+        'must_not' => [],
+    ];
+
+    /**
+     * 实例化一个构建链接
+     *
+     * @param ElasticsearchClient $client [description]
+     */
+    function __construct(ElasticsearchClient $client)
     {
-        # code...
+        $this->client = $client;
+    }
+
+    /**
+     * 指定索引名
+     *
+     * @param  string $value
+     * @return $this
+     */
+    public function index($value)
+    {
+        $this->index = $value;
+
+        return $this;
+    }
+
+    /**
+     * 指定type
+     *
+     * @param  string $value
+     * @return $this
+     */
+    public function type($value)
+    {
+        $this->type = $value;
+
+        return $this;
+    }
+
+
+
+
+
+
+
+    public function info()
+    {
+        return $this->client->info();
     }
 }
