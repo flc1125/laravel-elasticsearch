c609e09f98b076c32e40a8528489f418ff0e3ba9
Flc <four_leaf_clover@foxmail.com>
2018-10-03 12:46:42 +0800

    backup

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -3,6 +3,7 @@
 namespace Flc\Laravel\Elasticsearch\Query;
 
 use Elasticsearch\Client as ElasticsearchClient;
+use Illuminate\Database\Concerns\BuildsQueries;
 
 /**
  * Elasticsearch 查询构建类
@@ -11,6 +12,8 @@ use Elasticsearch\Client as ElasticsearchClient;
  */
 class Builder
 {
+    use BuildsQueries;
+
     /**
      * Elasticsearch Client
      *
@@ -30,7 +33,14 @@ class Builder
      *
      * @var string
      */
-    protected $type = 'docs';
+    protected $type;
+
+    /**
+     * 需要查询的字段
+     *
+     * @var array
+     */
+    protected $columns;
 
     /**
      * 搜寻条件
@@ -76,6 +86,80 @@ class Builder
         $this->type = $value;
 
         return $this;
+    }
+
+    /**
+     * 指定需要查询获取的字段
+     *
+     * @param  array|mixed  $columns
+     * @return $this
+     */
+    public function select($columns = ['*'])
+    {
+        $this->columns = is_array($columns) ? $columns : func_get_args();
+
+        return $this;
+    }
+
+    /**
+     * 返回数据
+     *
+     * @return Collection
+     */
+    public function get()
+    {
+        return collect($this->processSelect());
+    }
+
+    /**
+     * 处理搜寻结果
+     *
+     * @return array
+     */
+    protected function processSelect()
+    {
+        $result = $this->runSelect();
+
+        // 格式化数据...
+
+        return $result;
+    }
+
+    /**
+     * 执行搜寻
+     *
+     * @return array
+     */
+    protected function runSelect()
+    {
+        return $this->client->search(
+            $this->toParams()
+        );
+    }
+
+    /**
+     * 执行搜索的参数
+     *
+     * @return array
+     */
+    protected function toParams()
+    {
+        $params = [
+            'index' => $this->index,
+            'from'  => 0,
+            'size'  => 20,
+            'body'  => [
+                'query'   => [
+                    'bool' => $this->wheres,
+                ],
+                // '_source' => ['*'],
+                'sort'    => $this->orders,
+            ],
+        ];
+
+        if ($this->type) {
+            $params['type'] = $this->type;
+        }
     }
 
 
